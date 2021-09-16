7e2f34c00c0bd6372789e974c9b060dbc6be849e
Manali Shetye <manalis826@gmail.com>
2021-09-16 15:25:27 +0530

    added footer

--- src/views/footer.js
+++ src/views/footer.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export const footer = () => {
-    return (
-        <div>
-            This is footer
-        </div>
-    )
-}
+  return (
+    <div>
+      <footer className="bg-dark text-light py-3">
+        <p className="text-center">Copyright &copy; To Do App | 2021-2022</p>
+      </footer>
+    </div>
+  );
+};
