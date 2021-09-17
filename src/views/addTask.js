29ed3eacc2ca59ac16988c8b30b9fafd9879906a
Manali Shetye <manalis826@gmail.com>
2021-09-17 22:40:52 +0530

    jsx added to addTask component

--- src/views/addTask.js
+++ src/views/addTask.js
@@ -1,44 +1,61 @@
-import React from "react";
+import React, { useState }  from "react";
 
-export default function addTask() {
+export default function AddTask(props) {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (!title || !desc) {
+        alert("Title or Description cannot be blank");
+    }
+    else {
+        props.addTask(title, desc);
+        setTitle("");
+        setDesc("");
+    }
+}
   return (
     <div
       data-bs-spy="scroll"
       data-bs-target="#navScroll"
       data-bs-offset="0"
-      tabindex="0"
-      class="container my-4"
+      tabIndex="0"
+      className="container my-4"
     >
-      <div id="home" class="container my-4">
-        <h1 class="text-center">Welcome to To Do App</h1>
-        <div class="card my-5">
-          <div class="card-body">
-            <h5 class="card-title">Add title</h5>
-            <div class="form-group">
+      <div id="home" className="container my-4">
+        <h1 className="text-center">Welcome to To Do App</h1>
+        <div className="card my-5">
+          <div className="card-body">
+            <h5 className="card-title">Add title</h5>
+            <div className="form-group">
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="addTitle"
                 aria-describedby="title"
                 placeholder="Enter title"
+                value={title} onChange={(e)=>setTitle(e.target.value)}
               />
             </div>
-            <h5 class="card-title my-3">Add Task</h5>
-            <div class="form-group">
-              <textarea class="form-control" id="addTxt" rows="3"></textarea>
+            <h5 className="card-title my-3">Add Task</h5>
+            <div className="form-group">
+              <textarea className="form-control" id="addTxt" rows="3" value={desc} onChange={(e)=>setDesc(e.target.value)}></textarea>
             </div>
-            <div class="form-check my-2">
+            <div className="form-check my-2">
               <input
-                class="form-check-input"
+                className="form-check-input"
                 type="checkbox"
                 value=""
-                id="messageCheckbox"
+                id="messageCheckbox" onClick={(e)=>{
+                  props.onCheck(e);
+                }}
               />
-              <label class="form-check-label text-danger" for="messageCheckbox">
+              <label className="form-check-label text-danger" htmlFor="messageCheckbox">
                 Mark Task Important
               </label>
             </div>
-            <button class="btn btn-primary" id="addBtn">
+            <button className="btn btn-primary" id="addBtn" onClick={submit}>
               Add Task
             </button>
           </div>
